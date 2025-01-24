b09f84628e2ef7952c6142ac8af02ccef78bf0cd
Pascoal <195182831+pascoal-ghub@users.noreply.github.com>
2025-01-24 20:30:15 +0000

    Adicionado método somar

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -19,21 +19,6 @@ namespace Calculos
         {
             return x + y;
         }
-        /// <summary>
-        /// Operação de subtração
-        /// </summary>
-        /// <returns>Retorna o resultado da subtração de dois números </returns>
-        public static int Subtrair(int x, int y)
-        {
-            return x - y;
-        }
-        /// <summary>
-        /// Operação de multiplicação
-        /// </summary>
-        /// <returns>Retorna o resultado da multiplicação de dois números </returns>
-        public static int Multiplicar(int x, int y)
-        {
-            return x * y;
-        }
+        
     }
 }
